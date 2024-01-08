c455b95c7f210ef845f119d4b1d18a1b350524d6
axseem <tr1xn.gg@gmail.com>
2024-01-08 20:59:59 +0100

    feat: seed database

---
+++ cmd/seed/seed.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"context"
+	"database/sql"
+	"log"
+
+	_ "modernc.org/sqlite"
+
+	"github.com/axseem/blomple/database"
+)
+
+func main() {
+	sqlite, err := sql.Open("sqlite", "./sqlite.db")
+	if err != nil {
+		log.Fatal("failed to connect to database: ", err)
+	}
+	db := database.New(sqlite)
+
+	c := context.Background()
+	article := database.CreateArticleParams{
+		Title: "Hello World",
+		Body:  "This is the first article",
+	}
+
+	if err = db.CreateArticle(c, article); err != nil {
+		log.Fatal("failed to create article: ", err)
+	}
+}
